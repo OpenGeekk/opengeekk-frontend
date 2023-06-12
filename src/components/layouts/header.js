44f2426591087060e383ba8f8b5c50b0191f0207
Deep7452 <deependra7452singh@gmail.com>
2023-06-12 16:55:46 +0530

    Create Home page

--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -4,7 +4,7 @@ import Nav from './nav.jsx';
 const Header = () => {
     return (
         
-        <header className='border-b border-gray-400 sticky top-2 '>
+        <header className='border-b border-gray-400  sticky top-2 '>
             <Head>
                 <title>Explore OpenGeekk's</title>
                 <meta name="description" content="A promotional OpenGeeks official web application." />
